ea752ea0386a2754026041f08230657ac8a48aa1
jefferymichalski <jeffery.michalskiusmc@gmail.com>
2024-07-11 12:37:03 -0500

    added create for action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,15 @@ class ProductsController < ApplicationController
     render :index
   end
 
+  def create
+    @products = products.create(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
+    )
+  end
+
   def show
     @product = Product.find_by(id: params[:id])
     render :show
