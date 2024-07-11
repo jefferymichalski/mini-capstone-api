e20fafb654bf86661a62630ac069ba48c076621c
jefferymichalski <jeffery.michalskiusmc@gmail.com>
2024-07-11 13:51:29 -0500

    added test in the test controller

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -25,4 +25,20 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal ["id", "name", "price", "image_url", "description", "created_at", "updated_at"], data.keys
   end
+
+  test "update" do
+    product = Product.first
+    patch "/products/#{product.id}.json", params: { name: "Updated name" }
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal "Updated name", data["name"]
+  end
+
+  test "destroy" do
+    assert_difference "Product.count", -1 do
+      delete "/products/#{Product.first.id}.json"
+      assert_response 200
+    end
+  end
 end
