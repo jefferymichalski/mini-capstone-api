50e8b3212f6b3b814ab203a2ce5bc18fdcfacfd2
jefferymichalski <jeffery.michalskiusmc@gmail.com>
2024-07-11 11:47:33 -0500

    added restful convention to controllers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+  def index
+    @Products = Products.all
+    render :index
+  end
+
+  def show
+    @Product = Products.find_by(id: params[:id])
+    render :show
+  end
 end
