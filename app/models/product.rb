1711f94794ef35ce5f9ece7e9f5d4039cbdbf9f9
jefferymichalski <jeffery.michalskiusmc@gmail.com>
2024-07-15 14:12:31 -0500

    db migrations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
+  class Product < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :price, presence: true, numericality: { greater_than: 0 }
+    validates :description, length: { minimum: 1, maximum: 500 }
+    VALID_IMAGE_REGEX = /\.(gif|jpg|jpeg|png)\z/i
+    validates :image_url, format: { with: VALID_IMAGE_REGEX, message: "must be a URL for GIF, JPG, JPEG, or PNG image." }
+  end
+
   def is_discounted?
     price <= 10
   end
---
+++ db/migrate/20240715183119_change_descriptio_to_text.rb
@@ -0,0 +1,5 @@
+class ChangeDescriptioToText < ActiveRecord::Migration[7.1]
+  def change
+    change_column :products, :description, :text
+  end
+end
