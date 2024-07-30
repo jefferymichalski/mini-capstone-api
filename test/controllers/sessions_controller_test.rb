ae83f413a51eb4b43cfe0a2818a1772977493e2f
jefferymichalski <jeffery.michalskiusmc@gmail.com>
2024-07-30 13:51:22 -0500

    fixed routes

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "create" do
+  est "create" do
     post "/users.json", params: { name: "Test", email: "test@test.com", password: "password", password_confirmation: "password" }
     post "/sessions.json", params: { email: "test@test.com", password: "password" }
     assert_response 201
